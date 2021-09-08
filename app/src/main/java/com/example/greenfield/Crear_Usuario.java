9ee8d1d49324b80c64d3f4d519203cb0476d1243
Jose Cabello <jmcabelloquispe@gmail.com>
2021-09-07 23:08:40 -0500

    initial commit

---
+++ app/src/main/java/com/example/greenfield/Crear_Usuario.java
@@ -0,0 +1,17 @@
+package com.example.greenfield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear__usuario);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/greenfield/Login.java
@@ -0,0 +1,17 @@
+package com.example.greenfield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
